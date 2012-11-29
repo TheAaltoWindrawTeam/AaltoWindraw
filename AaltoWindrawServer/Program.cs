e7f190c92deaa4afe2a135478b54392d3e10ab14
ArthurTemplé <arthur.temple@gmail.com>
2012-11-29 20:16:24 +0100

    Add a placeholder server project

---
+++ AaltoWindrawServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AaltoWindrawServer.Server;
+
+namespace AaltoWindrawServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Server server = new Server.Server();
+            Console.WriteLine("Welcome to AaltoWindraw server application");
+            if (!server.start())
+            {
+                Console.WriteLine("Server failed to start");
+            }
+            Console.WriteLine("That's all, folks! Type anything to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AaltoWindrawServer/Server/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AaltoWindrawServer.Server
+{
+    class Server
+    {
+        public Server()
+        {
+
+        }
+
+        public bool start()
+        {
+            return false;
+        }
+    }
+}
